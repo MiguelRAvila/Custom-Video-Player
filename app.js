557b36d1a7e121fab9cb49ad6dc5dd7cb2633a3e
Miguel R. Ávila <miguelravila@outlook.com>
2020-04-24 17:08:38 -0500

    Functions

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 const video = document.getElementById("video");
 const play = document.getElementById("play");
 const stop = document.getElementById("stop");
-const progre = document.getElementById("progre");
-const time = document.getElementById("time");
+const progre = document.getElementById("progress");
+const time = document.getElementById("timestapm");
+
+// Play and Pause Video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+function updateIcon() {
+  return true;
+}
+function stopVideo() {
+  return true;
+}
+
+// Update Progress and TimeStamp
+function updateProgress() {
+  return true;
+}
+function setVideoProgress() {
+  return true;
+}
 
 // Event Listeners
-video.addEventListener("click", toggleVideoStatus());
+video.addEventListener("click", toggleVideoStatus);
 video.addEventListener("pause", updateIcon);
-video.addEventListener("play", updateIcon());
-video.addEventListener("timeupdate", updateProgress());
+video.addEventListener("play", updateIcon);
+video.addEventListener("timeupdate", updateProgress);
 
-play.addEventListener("click", toggleVideoStatus());
-stop.addEventListener("click", stopVideo());
-progre.addEventListener("change", setVideoProgress());
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+progre.addEventListener("change", setVideoProgress);
