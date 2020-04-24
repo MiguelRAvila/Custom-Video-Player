ff43c6ef53920d03372556fdf61eac3dd61b3d98
Miguel R. Ávila <miguelravila@outlook.com>
2020-04-24 16:55:24 -0500

    Event Listeners

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const progre = document.getElementById("progre");
 const time = document.getElementById("time");
+
+// Event Listeners
+video.addEventListener("click", toggleVideoStatus());
+video.addEventListener("pause", updateIcon);
+video.addEventListener("play", updateIcon());
+video.addEventListener("timeupdate", updateProgress());
+
+play.addEventListener("click", toggleVideoStatus());
+stop.addEventListener("click", stopVideo());
+progre.addEventListener("change", setVideoProgress());
